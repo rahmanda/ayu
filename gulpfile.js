0d8c57cb7c5c676769e6dbcf47d9664aeb169479
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2016-10-09 20:46:35 +0700

    add initialize scss framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp        = require("gulp");
+var serve       = require("gulp-serve");
 var sass        = require("gulp-sass");
 var cssnano     = require("gulp-cssnano");
 var concat      = require("gulp-concat");
@@ -12,50 +13,84 @@ var browserify  = require("browserify");
 
 var ROOTPATH = "/";
 var SERVER   = "http://localhost:8000";
+var PREFIX_PATH = {
+  src: 'src',
+  dist: 'dist'
+};
 var PATH = {
-  sass   : "assets/scss/**/*.scss",
-  view   : "views/**/*.pug",
-  js     : "assets/js/**/*.js",
-  dist : {
-    css : "public/assets/css",
-    js  : "public/assets/js",
-    view: "public/"
+  css    : {
+    entry: PREFIX_PATH.src + "/views/demo.css",
+    dist:  PREFIX_PATH.dist + "/assets/css"
+  },
+  sass   : {
+    entry: PREFIX_PATH.src + "/scss/app.scss",
+    src:   PREFIX_PATH.src + "/scss/**/*.scss",
+    dist:  PREFIX_PATH.dist + "/assets/css"
+  },
+  view   : {
+    entry: PREFIX_PATH.src + "/views/index.pug",
+    src:   PREFIX_PATH.src + "/views/**/*.pug",
+    dist:  PREFIX_PATH.dist
+  },
+  js     : {
+    entry: PREFIX_PATH.src + "/js/app.js",
+    src:   PREFIX_PATH.src + "/js/**/*.js",
+    dist:  PREFIX_PATH.dist + "/assets/js"
   }
 };
 
 // Static server + watching asset files
-gulp.task('serve', ['sass', 'browserify'], function () {
+gulp.task('serve', ['sass', 'browserify', 'pug', 'democss'], function() {
   browserSync.init({
     proxy: SERVER
   });
 
-  gulp.watch(PATH.sass, ['sass']);
-  gulp.watch(PATH.js, ['browserify'], browserSync.reload);
-  gulp.watch(PATH.view).on('change', browserSync.reload);
+  gulp.watch(PATH.sass.src, ['sass']);
+  gulp.watch(PATH.css.entry, ['democss']);
+  gulp.watch(PATH.js.src, ['browserify'], browserSync.reload);
+  gulp.watch(PATH.view.src, ['pug'], browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-  return gulp.src(PATH.sass)
+  return gulp.src(PATH.sass.src)
          .pipe(sass())
          .pipe(cssnano())
-         .pipe(gulp.dest(PATH.dist.css))
+         .pipe(gulp.dest(PATH.sass.dist))
          .pipe(browserSync.stream());
 });
 
+// Compile all js files into one file
 gulp.task('browserify', function() {
-  return browserify('assets/js/app.js')
+  return browserify(PATH.js.entry)
     .bundle()
     .pipe(source('app.min.js'))
     .pipe(buffer())
     .pipe(uglify())
-    .pipe(gulp.dest(PATH.dist.js));
+    .pipe(gulp.dest(PATH.js.dist));
 });
 
+// Compile all pug files into one file
 gulp.task('pug', function() {
-  return gulp.src('views/*.pug')
+  return gulp.src(PATH.view.entry)
     .pipe(pug())
-    .pipe(gulp.dest(PATH.dist.view));
+    .pipe(gulp.dest(PATH.view.dist));
 });
 
+// Minify and clone demo css
+gulp.task('democss', function() {
+  return gulp.src(PATH.css.entry)
+    .pipe(cssnano())
+    .pipe(gulp.dest(PATH.css.dist))
+    .pipe(browserSync.stream());
+});
+
+// Create static website server
+// need to be executed separately from main task
+gulp.task('server', serve({
+    root: ['dist'],
+    port: 8000
+}));
+
+// Main task
 gulp.task('default', ['serve']);
---
+++ src/js/app.js
@@ -0,0 +1,1 @@
+var Box = require('t3js');
