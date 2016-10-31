59f6224e1105342e4ea4ea2c11a21212a3777f26
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2016-10-31 16:36:41 +0700

    add dropdown

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,8 @@ var Box = require('t3js');
 
 Box.Application.addModule('modal', require('./modules/modal'));
 Box.Application.addModule('open-modal', require('./modules/open-modal'));
+Box.Application.addModule('actions', require('./modules/action'));
+
+Box.Application.addBehavior('dropdown', require('./behaviors/dropdown'));
 
 Box.Application.init();
